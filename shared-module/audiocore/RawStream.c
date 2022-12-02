f003c54e4d0d919ba85492f91428911a7be43176
Rybec Arethdar <rybecarethdar@gmail.com>
2022-12-01 16:00:05 -0900

    Changed all rawsample references to rawstream and added buffer allocation (based on code from WaveFile.c)

--- shared-module/audiocore/RawStream.c
+++ shared-module/audiocore/RawStream.c
@@ -31,48 +31,68 @@
 
 #include "shared-module/audiocore/RawSample.h"
 
-void common_hal_audioio_rawsample_construct(audioio_rawsample_obj_t *self,
+void common_hal_audioio_rawstream_construct(audioio_rawstream_obj_t *self,
     uint8_t *buffer,
     uint32_t len,
     uint8_t bytes_per_sample,
     bool samples_signed,
     uint8_t channel_count,
     uint32_t sample_rate) {
-    self->buffer = buffer;
+
+    // Try to allocate two buffers, one will be loaded from file and the other
+    // DMAed to DAC.
+    if (buffer_size) {
+        self->len = buffer_size / 2;
+        self->buffer = buffer;
+        self->second_buffer = buffer + self->len;
+    } else {
+        self->len = 256;
+        self->buffer = m_malloc(self->len, false);
+        if (self->buffer == NULL) {
+            common_hal_audioio_rawstream_deninit(self);
+            m_malloc_fail(self->len);
+        }
+
+        self->second_buffer = m_mallon(self->len, false);
+        if (self->second_buffer == NULL) {
+            common_hal_audioio_rawstream_deinit(self);
+            m_malloc_fail(self->len);
+        }
+    }
+
     self->bits_per_sample = bytes_per_sample * 8;
     self->samples_signed = samples_signed;
-    self->len = len;
     self->channel_count = channel_count;
     self->sample_rate = sample_rate;
 }
 
-void common_hal_audioio_rawsample_deinit(audioio_rawsample_obj_t *self) {
+void common_hal_audioio_rawstream_deinit(audioio_rawstream_obj_t *self) {
     self->buffer = NULL;
 }
-bool common_hal_audioio_rawsample_deinited(audioio_rawsample_obj_t *self) {
+bool common_hal_audioio_rawstream_deinited(audioio_rawstream_obj_t *self) {
     return self->buffer == NULL;
 }
 
-uint32_t common_hal_audioio_rawsample_get_sample_rate(audioio_rawsample_obj_t *self) {
+uint32_t common_hal_audioio_rawstream_get_sample_rate(audioio_rawstream_obj_t *self) {
     return self->sample_rate;
 }
-void common_hal_audioio_rawsample_set_sample_rate(audioio_rawsample_obj_t *self,
+void common_hal_audioio_rawstream_set_sample_rate(audioio_rawstream_obj_t *self,
     uint32_t sample_rate) {
     self->sample_rate = sample_rate;
 }
-uint8_t common_hal_audioio_rawsample_get_bits_per_sample(audioio_rawsample_obj_t *self) {
+uint8_t common_hal_audioio_rawstream_get_bits_per_sample(audioio_rawstream_obj_t *self) {
     return self->bits_per_sample;
 }
-uint8_t common_hal_audioio_rawsample_get_channel_count(audioio_rawsample_obj_t *self) {
+uint8_t common_hal_audioio_rawstream_get_channel_count(audioio_rawstream_obj_t *self) {
     return self->channel_count;
 }
 
-void audioio_rawsample_reset_buffer(audioio_rawsample_obj_t *self,
+void audioio_rawstream_reset_buffer(audioio_rawstream_obj_t *self,
     bool single_channel_output,
     uint8_t channel) {
 }
 
-audioio_get_buffer_result_t audioio_rawsample_get_buffer(audioio_rawsample_obj_t *self,
+audioio_get_buffer_result_t audioio_rawstream_get_buffer(audioio_rawstream_obj_t *self,
     bool single_channel_output,
     uint8_t channel,
     uint8_t **buffer,
@@ -86,7 +106,7 @@ audioio_get_buffer_result_t audioio_rawsample_get_buffer(audioio_rawsample_obj_t
     return GET_BUFFER_DONE;
 }
 
-void audioio_rawsample_get_buffer_structure(audioio_rawsample_obj_t *self, bool single_channel_output,
+void audioio_rawstream_get_buffer_structure(audioio_rawstream_obj_t *self, bool single_channel_output,
     bool *single_buffer, bool *samples_signed,
     uint32_t *max_buffer_length, uint8_t *spacing) {
     *single_buffer = true;
--- shared-module/audiocore/RawStream.h
+++ shared-module/audiocore/RawStream.h
@@ -35,24 +35,25 @@
 typedef struct {
     mp_obj_base_t base;
     uint8_t *buffer;
+    uint8_t *second_buffer;
     uint32_t len;
     uint8_t bits_per_sample;
     bool samples_signed;
     uint8_t channel_count;
     uint32_t sample_rate;
-} audioio_rawsample_obj_t;
+} audioio_rawstream_obj_t;
 
 
 // These are not available from Python because it may be called in an interrupt.
-void audioio_rawsample_reset_buffer(audioio_rawsample_obj_t *self,
+void audioio_rawstream_reset_buffer(audioio_rawstream_obj_t *self,
     bool single_channel_output,
     uint8_t channel);
-audioio_get_buffer_result_t audioio_rawsample_get_buffer(audioio_rawsample_obj_t *self,
+audioio_get_buffer_result_t audioio_rawstream_get_buffer(audioio_rawstream_obj_t *self,
     bool single_channel_output,
     uint8_t channel,
     uint8_t **buffer,
     uint32_t *buffer_length);                                                      // length in bytes
-void audioio_rawsample_get_buffer_structure(audioio_rawsample_obj_t *self, bool single_channel_output,
+void audioio_rawsample_get_buffer_structure(audioio_rawstream_obj_t *self, bool single_channel_output,
     bool *single_buffer, bool *samples_signed,
     uint32_t *max_buffer_length, uint8_t *spacing);
 
